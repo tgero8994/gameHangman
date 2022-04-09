90630e527181a1ae7cdcdb19875d7b2d206883be
tgero8994 <tgero8994@students.mc3.edu>
2022-04-09 18:08:25 -0400

    Updated comments
    Updated the comments in both files

--- Hangman.java
+++ Hangman.java
@@ -6,29 +6,24 @@ import java.lang.*;
 public class Hangman extends HangmanMethods
 {
    /** --------------------------------------------------------------------CHECKLIST OF HOW TO START-------------------------------------------------------------------
-
-     DONE 1. Chooses a random word amongst a list of words in a file (then change to an online file)
-      2. Print the word in an unknown state using _ _ _ _ _ for each letter before asking for input
-      3. Ask for a user input of only one letter no matter if upper or lower case
-      4. Check if the letter is in the randomly selected word
-            If yes reprint the word but have _ for each unknown letter and fill in the new known letters **(For words with multiple of the same letter fill in all of that instance)**
-            If no say "wrong" and add a count to the amount needed to lose (head, body, left arm, right arm, left leg, right leg)
-      5. Eliminate the guessed letter from the 26 possible letters so that it cannot be guessed again.
-      6. Repeat step 2 and 3 until the word is fully guessed and increment onto the unkown word for each new known letter
-      7. If all letters are guessed correct reprint the final word and tell the user "You win"
-      8. If all 6 body parts are built or the count gets to 6 the game ends and prints "You Lose"
+      
+     >1. Chooses a random word amongst a list of words in a file
+     >2. Print the word in an unknown state using _ _ _ _ _ for each letter before asking for input
+     >3. Ask for a user input of only one letter no matter if upper or lower case
+     >4. Check if the letter is in the randomly selected word
+     >      If yes reprint the word but have _ for each unknown letter and fill in the new known letters **(For words with multiple of the same letter fill in all of that instance)**
+     >      If no say "wrong" and add a count to the amount needed to lose (head, body, left arm, right arm, left leg, right leg)
+     >5. Eliminate the guessed letter from the 26 possible letters so that it cannot be guessed again.
+     >6. Repeat step 2 and 3 until the word is fully guessed and increment onto the unkown word for each new known letter
+     >7. If all letters are guessed correct reprint the final word and tell the user "You win"
+     >8. If all 6 body parts are built or the count gets to 6 the game ends and prints "You Lose"
       
       
      -------------------------------------------------------------------------------THINGS TO ADD--------------------------------------------------------------------------------------------------
-      
-        > 1. IF THE WORD IS COMPLETLY SOLVED -> STOP THE LOOP AND PRINT THE AMOUNT OF GUESSES USED AND THEN FINAL WORD (LOOK AT NUMBER 6)
-          2. ADD A LETTER BANK OF THE LETTERS ALREADY GUESSED AND THE LETTERS STILL AVAILABLE TO GUESS (MAYBE USE AN ARRAY WITH ALL LETTERS AND REMOVE AFTER SELECTING)
-               ONLY ALLOW THE USER TO PICK FROM THAT ARRAY WHICH INCLUDES THE LETTERS LEFT TO GUESS
-          3. DONT ALLOW TO ENTER A VALUE ALREADY GUESSED
-        > 4. CONVERT THE USER INPUT TO LOWER CASE
-        > 5. STOP AND SAY YOU LOSE AFTER GETTING 6 INCORRECT LETTERS (HANGMAN CHARCTER PARTS)
-        > 6. WRITE IF STATEMENT THAT SAYS IF theWord INCLUDES NO '_' VALUES THEN THE WORD IS SOLVED
-          7. CHANGE UPPERBOUND TO CHANGE SIZE DEPENDING ON THE AMOUNT OF WORDS
+          1. ADD API
+            - Change the random word generator by getting a word from an API
+            - add a sorter to pick a word with only a certain amount of letters
+          2. CHANGE UPPERBOUND TO CHANGE SIZE DEPENDING ON THE AMOUNT OF WORDS / MAKE THE API CONNECT
       
    */
    public static void main(String[] args)throws Exception
