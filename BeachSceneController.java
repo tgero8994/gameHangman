f250a8fa68424ca0813624455bbbc75611f9a525
ckerns4155 <103332833+ckerns4155@users.noreply.github.com>
2022-04-20 20:39:47 -0400

    Added BeachSceneController

---
+++ BeachSceneController.java
@@ -0,0 +1,66 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private ImageView beachBody;
+
+    @FXML
+    private ImageView beachHead;
+
+    @FXML
+    private ImageView beachLeftArm;
+
+    @FXML
+    private ImageView beachLeftLeg;
+
+    @FXML
+    private ImageView beachPicture;
+
+    @FXML
+    private ImageView beachRightArm;
+
+    @FXML
+    private ImageView beachRightLeg;
+
+    @FXML
+    private Button checkLetter;
+
+    @FXML
+    private ImageView home;
+
+    @FXML
+    private TextField letter1;
+
+    @FXML
+    private TextField letter2;
+
+    @FXML
+    private TextField letter3;
+
+    @FXML
+    private TextField letter4;
+
+    @FXML
+    private TextField letter5;
+
+    @FXML
+    private TextField letterGuess;
+
+    @FXML
+    private TextArea lettersGuessedList;
+
+    @FXML
+    private Button resetWord;
+
+    @FXML
+    void test(KeyEvent event) {
+
+    }
+
+}
