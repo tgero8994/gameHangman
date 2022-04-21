5a1c457d425b7aa2684c466546b3f18f2928266c
ckerns4155 <103332833+ckerns4155@users.noreply.github.com>
2022-04-20 21:03:25 -0400

    Added Controllers for WesternScene and OpeningScene

---
+++ OpeningSceneController.java
@@ -0,0 +1,23 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.image.ImageView;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private ImageView beachPicture;
+
+    @FXML
+    private Button play;
+
+    @FXML
+    private RadioButton setting1;
+
+    @FXML
+    private RadioButton setting2;
+
+    @FXML
+    private ImageView westernPicture;
+
+}
