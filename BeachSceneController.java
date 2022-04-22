d0c4c6d24dacaa4af9220fd1a839055fa22700cb
tgero8994 <tgero8994@students.mc3.edu>
2022-04-21 23:23:18 -0400

    Add method to make character appear after each incorrect guess

--- BeachSceneController.java
+++ BeachSceneController.java
@@ -10,12 +10,10 @@ import java.lang.*;
 
 public class BeachSceneController {
     
-    private static final ArrayList<Character> theWord = new ArrayList<>();
     private static final ArrayList<Character> guessedLettersArray = new ArrayList<>(); // list of the letters the user already guessed
-    private static char letterGuess; // the user's inputted guess
-    private static String currentWord;  // randomly picked word
+    private static char firstLetterInGuess; // the user's inputted guess
+    private static String currentWord = "hello";  // randomly picked word
     private static int incorrectGuesses = 0;  // keeps the amount of times an incorrect letter was guessed
-    private static final int TRIESGIVEN = 6;  // number of tries allowed before user loses
 
     @FXML
     private ImageView beachBody;
@@ -71,14 +69,11 @@ public class BeachSceneController {
     @FXML
     void checkLetterButton(ActionEvent event) {
       // checks if the first letter in the guess letter text box is a valid letter and then places an uppercase value into the guessedLettersArray if that letter isn't already there
-      char firstLetterInGuess = letterGuessBox.getText().charAt(0); // sets only the first letter in the guess box as a char so it can be passed through isALetter() method
+      firstLetterInGuess = letterGuessBox.getText().charAt(0); // sets only the first letter in the guess box as a char so it can be passed through isALetter() method
       if(isALetter(firstLetterInGuess))
       {
-         if(!guessedLettersArray.contains(firstLetterInGuess)){
-            guessedLettersArray.add(firstLetterInGuess);
-         }
-         String list = guessedLettersArray.toString().toUpperCase();
-         lettersGuessedList.setText(list);
+         isTheLetterGuessCorrect();
+         buildHangmanCharcter();
       }
     }
 
@@ -97,5 +92,53 @@ public class BeachSceneController {
       // char guess = c.toUpperCase();
       return c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z';
    }
-
+   
+   public void addLettersToListOfAlreadyGuessed()
+   {
+      // Adds letters to guess box
+         if(!guessedLettersArray.contains(firstLetterInGuess)){
+            guessedLettersArray.add(firstLetterInGuess);
+         }
+         String list = guessedLettersArray.toString().toUpperCase();
+         lettersGuessedList.setText(list);
+   }
+   
+   // if word contains letter guessed fill it in aproprate letter box
+   // if word doesn't contain the letter guessed +1 incorrect guesses
+   // if the array of already guessed contains letter -1 incorrect guesses
+   public void isTheLetterGuessCorrect()
+   {
+      String shc = guessedLettersArray.toString();
+      // letter not in word and not in list +1
+      if(!currentWord.contains("" + firstLetterInGuess) && !shc.contains("" + firstLetterInGuess))
+      {
+         incorrectGuesses++;
+         System.out.println("Letter Not in word " + incorrectGuesses);
+      }
+      // letter in word
+      else if(currentWord.contains("" + firstLetterInGuess) && !shc.contains("" + firstLetterInGuess)){
+         System.out.println("Letter in word " + incorrectGuesses);
+      }
+      // letter already guessed
+      else
+         System.out.println("Letter Already Guessed " + incorrectGuesses);
+      addLettersToListOfAlreadyGuessed();
+   }
+   
+   public void buildHangmanCharcter()
+   {
+      if(incorrectGuesses==1)
+         beachHead.setVisible(true);
+      else if(incorrectGuesses==2)
+         beachBody.setVisible(true);
+      else if(incorrectGuesses==3)
+         beachLeftArm.setVisible(true);
+      else if(incorrectGuesses==4)
+         beachRightArm.setVisible(true);
+      else if(incorrectGuesses==5)
+         beachLeftLeg.setVisible(true);
+      else if(incorrectGuesses==6)
+         beachRightLeg.setVisible(true);
+      
+   }
 }
